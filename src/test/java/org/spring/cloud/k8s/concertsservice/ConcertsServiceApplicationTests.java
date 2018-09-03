10d0bb2cfa9adb9360fe1f5632ae30ce36c691ab
salaboy <salaboy@gmail.com>
2018-09-03 10:31:25 +0100

    adding embeded mongodb for tests

--- src/test/java/org/spring/cloud/k8s/concertsservice/ConcertsServiceApplicationTests.java
+++ src/test/java/org/spring/cloud/k8s/concertsservice/ConcertsServiceApplicationTests.java
@@ -2,11 +2,16 @@ package org.spring.cloud.k8s.concertsservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest()
+@TestPropertySource("classpath:test-application.properties")
+@DataMongoTest
 public class ConcertsServiceApplicationTests {
 
 	@Test
