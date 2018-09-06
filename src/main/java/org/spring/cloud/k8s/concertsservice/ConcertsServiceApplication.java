42fc0e3815eca67d1f397aadf2032227613d4714
salaboy <salaboy@gmail.com>
2018-09-06 12:05:23 +0100

    cleaning up config

--- src/main/java/org/spring/cloud/k8s/concertsservice/ConcertsServiceApplication.java
+++ src/main/java/org/spring/cloud/k8s/concertsservice/ConcertsServiceApplication.java
@@ -2,10 +2,10 @@ package org.spring.cloud.k8s.concertsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -15,16 +15,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableDiscoveryClient
 public class ConcertsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConcertsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertsServiceApplication.class, args);
+    }
 
-	@Configuration
-	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests().anyRequest().permitAll()
-					.and().csrf().disable();
-		}
-	}
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
 }
