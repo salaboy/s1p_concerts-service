baa0ac3b65a476c03474486c9949ac78e8535fde
salaboy <salaboy@gmail.com>
2018-09-06 11:26:23 +0100

    adding admin client

--- src/main/java/org/spring/cloud/k8s/concertsservice/ConcertsServiceApplication.java
+++ src/main/java/org/spring/cloud/k8s/concertsservice/ConcertsServiceApplication.java
@@ -2,9 +2,12 @@ package org.spring.cloud.k8s.concertsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableMongoAuditing
@@ -14,5 +17,14 @@ public class ConcertsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConcertsServiceApplication.class, args);
+	}
+
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
 	}
 }
