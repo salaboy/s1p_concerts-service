d3b26a7c553f93da0c0ea4f44130192137304338
salaboy <salaboy@gmail.com>
2018-09-18 12:14:34 +0100

    adding refresh scope

--- src/main/java/org/spring/cloud/k8s/concertsservice/services/ConcertServiceImpl.java
+++ src/main/java/org/spring/cloud/k8s/concertsservice/services/ConcertServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -17,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@RefreshScope
 public class ConcertServiceImpl implements ConcertService {
 
     private static final Logger log = LoggerFactory.getLogger(ConcertServiceImpl.class);
