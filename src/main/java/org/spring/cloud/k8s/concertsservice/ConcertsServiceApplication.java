d845be006bd364f9d042baa4668bca4595806078
salaboy <salaboy@gmail.com>
2018-09-18 17:02:54 +0100

    adding @enabledscheduled

--- src/main/java/org/spring/cloud/k8s/concertsservice/ConcertsServiceApplication.java
+++ src/main/java/org/spring/cloud/k8s/concertsservice/ConcertsServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableMongoAuditing
 @EnableMongoRepositories
 @EnableDiscoveryClient
+@EnableScheduling
 public class ConcertsServiceApplication {
 
     @Configuration
